ecbbc77ed23bb0d14217fdf2df94dfe77d2c6ca4
Azfari Ikhsan <ikhsan1310@gmail.com>
2025-07-10 06:21:11 +0700

    Initial commit

---
+++ lib/halaman_utama/screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    Center(
+      child: Text(
+        'Home Page',
+        style: TextStyle(fontSize: 20),
+      ),
+    ),
+    Center(
+      child: Text(
+        'Search Page',
+        style: TextStyle(fontSize: 20),
+      ),
+    ),
+    Center(
+      child: Text(
+        'Profile Page',
+        style: TextStyle(fontSize: 20),
+      ),
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Main Page'),
+      ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: MainPage(),
+  ));
+}
