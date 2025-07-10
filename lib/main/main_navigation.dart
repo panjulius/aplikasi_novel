47fad28a98076b8b0781f21f1d46e22036aa1bbd
Azfari Ikhsan <ikhsan1310@gmail.com>
2025-07-10 08:28:21 +0700

    tambah halaman dan edit nama

---
+++ lib/main/main_navigation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
